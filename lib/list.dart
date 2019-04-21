bff357876b5e35c0c67105939cc942b5d0378ca2
Adi Gunawan <adi_gunawan@live.com>
2019-04-21 16:55:27 +0800

    file di widget

---
+++ lib/list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatefulWidget {
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('List Page'),
+      ),
+      body: ListView(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
